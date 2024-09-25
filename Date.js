14e23eb7533d87c7360a836b74003fac90deeb20
dunglt94 <tuandung9594@gmail.com>
2024-09-25 16:53:33 +0700

    first add

---
+++ Date.js
@@ -0,0 +1,39 @@
+class Date {
+    day; month; year;
+
+    constructor(day, month, year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    getDay() {
+        return this.day;
+    }
+    getMonth() {
+        return this.month;
+    }
+    getYear() {
+        return this.year;
+    }
+    setDay(day) {
+        this.day = day;
+    }
+    setMonth(month) {
+        this.month = month;
+    }
+    setYear(year) {
+        this.year = year;
+    }
+    setDate(day, month, year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    toString() {
+        return `${this.day}/${this.month}/${this.year}`;
+    }
+}
+
+
+
---
+++ js.js
@@ -0,0 +1,2 @@
+let birthday = new Date(9, 5, 1994)
+alert(birthday.toString());
